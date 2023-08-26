27cd55c575ba2fbe73f53efc2cf82d9c1edb401f
umeshjain1999 <ujain2601@gmail.com>
2023-08-26 17:18:24 +0530

    feat: initial commit

---
+++ app/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+  return(
+    <footer>
+      <a href="https://opensource.org/license/mit/" target="_blank">MIT</a>
+      {new Date().getFullYear()}-present <span> &#169;</span>
+      <a href="https://github.com/umeshjain1999" target="_blank">umesh</a>
+    </footer>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,42 @@
+import { API_ENDPOINT } from "@/constant";
+import Image from "next/image";
+import Pagination from "./pagination";
+import Card from "./card";
+
+async function getCharacters (page) {
+  const res = await fetch(`${API_ENDPOINT}/character/?page=${page}`,{
+    cache: "no-store"
+  });
+  return res.json();
+}
+
+export default async function Home({searchParams}) {
+  
+  const {page = 1} = searchParams
+  const characters = await getCharacters(page)
+  const {results,info} = characters
+  
+  return (
+    <main className="main">
+      <Image
+        src="/logo.png"
+        alt="Rick and Morty"
+        width={"300"}
+        height={"100"}
+        style={{
+          marginBottom: "2rem",
+        }}
+      />
+      <ul className="card-wrapper">
+        {results && results.map(data => {
+          return (
+            <li className="card" key={`${data.id}`}>
+              <Card id={data.id} name={data?.name || "Unknown"} image={data.image} />
+            </li>
+          )
+        })}
+      </ul>
+      <Pagination page = {page} pages={info?.pages} />
+    </main>
+  )
+}
